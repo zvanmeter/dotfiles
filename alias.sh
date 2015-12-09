1434fb2d7dcf9474f47e1900ee99d54546aafc2f
Zach VanMeter <zvanmeter@gmail.com>
2015-12-09 16:39:33 -0600

    Started gathering up my commonly used aliases

---
+++ alias.sh
@@ -0,0 +1,8 @@
+alias zr='source ~/.zshrc'
+alias l='ls -lAhF'
+alias ll='l -tr'
+alias tmux='env TERM=screen-256color tmux'
+alias opn='gnome-open .' > /dev/null 2>&1
+alias clip='clipcopy' #clipcopy is a ohmyzsh builtin fuction
+
+my_zsh_plugins=(common-aliases git sudo web-search debian tmux nyan copydir)
