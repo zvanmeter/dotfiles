a151dc1a77c214529a522658c170fa8ca635cc82
Zach VanMeter <zvanmeter@gmail.com>
2016-07-03 16:10:53 -0500

    Fun new aliases

--- alias.sh
+++ alias.sh
@@ -6,6 +6,13 @@ alias l='ls -lAhF'
 alias ll='l -tr'
 alias tmux='env TERM=screen-256color tmux'
 alias opn='gnome-open .' > /dev/null 2>&1
-alias clip='clipcopy' #clipcopy is a ohmyzsh builtin fuction
+alias clip='clipcopy' #ohmyzsh builtin fuction
+alias paste='clippaste' #overrides Unix paste, ohmyzsh function
+alias csv='sed "s/,,/, ,/g;s/,,/, ,/g" | column -s, -t'
+alias tsv='sed "s/^\t/ \t/g;s/\t\t/\t \t/g;s/\t\t/\ty\t/g" | column -s$'"'"'\t'"'"' -t'
+alias sqlist='paste | awk '"'"'{print "\x27"$0"\x27,"}'"'"' | sed "$ s/.$//" | clip && echo
+"$(paste | wc -l) lines modfied in copybuffer"'
+alias sqlint='paste | awk '"'"'{print $0","}'"'"' | sed "$ s/.$//" | clip && echo "$(paste | wc
+-l) lines modfied in copybuffer"'
 
 my_zsh_plugins=(common-aliases git sudo web-search debian tmux nyan copydir)
