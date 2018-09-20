e205956f1e65bb7afcc5c1aa66eb185d35f550c8
Zach VanMeter <zvanmeter@gmail.com>
2018-09-19 22:58:24 -0500

    Show default branch and handle no pipenv

--- scripts/activate_pipenv_environment.sh
+++ scripts/activate_pipenv_environment.sh
@@ -1,5 +1,5 @@
-pipenvVirt="$(pipenv --venv)/bin/activate"
-if [ -e "${pipenvVirt}" ]; then
+if pipenv --venv > /dev/null 2>&1; then
+  pipenvVirt="$(pipenv --venv)/bin/activate"
   source "${pipenvVirt}"
   export PIPENV_ACTIVE=1
 fi
--- scripts/create_pipenv_direnv.sh
+++ scripts/create_pipenv_direnv.sh
@@ -3,8 +3,10 @@ set -eu
 contents="if [ -e "../.envrc" ]; then
   source_env ..
 fi
+export PYENV_ROOT="$HOME/.pyenv"
 source pipenvenv
 git fetch --all --prune || true
+echo \"Origin Default: \$(git symbolic-ref refs/remotes/origin/HEAD  --short | cut -d/ -f2-)\"
 pyshow"
 
 echo "${contents}" > .envrc
