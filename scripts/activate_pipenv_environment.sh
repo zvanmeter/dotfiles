ba0d2439460c8e91fb17ee35c9bcc1c736e89828
Zach VanMeter <zvanmeter@gmail.com>
2018-09-19 23:19:32 -0500

    Move pyenv from direnv to pipenv script

--- scripts/activate_pipenv_environment.sh
+++ scripts/activate_pipenv_environment.sh
@@ -1,5 +1,6 @@
+export PYENV_ROOT="$HOME/.pyenv"
 if pipenv --venv > /dev/null 2>&1; then
+  export PIPENV_ACTIVE=1
   pipenvVirt="$(pipenv --venv)/bin/activate"
   source "${pipenvVirt}"
-  export PIPENV_ACTIVE=1
 fi
