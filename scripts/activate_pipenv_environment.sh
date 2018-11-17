2fe793c9f8f9d6c22fa1f32123d5707922a9288a
Zach VanMeter <zvanmeter@gmail.com>
2018-11-16 18:26:16 -0600

    .env file improvements

--- scripts/activate_pipenv_environment.sh
+++ scripts/activate_pipenv_environment.sh
@@ -1,4 +1,5 @@
 export PYENV_ROOT="$HOME/.pyenv"
+export PYTHONUNBUFFERED=1
 if pipenv --venv > /dev/null 2>&1; then
   export PIPENV_ACTIVE=1
   pipenvVirt="$(pipenv --venv)/bin/activate"
--- scripts/create_pipenv_direnv.sh
+++ scripts/create_pipenv_direnv.sh
@@ -3,6 +3,11 @@ set -eu
 contents="if [ -e "../.envrc" ]; then
   source_env ..
 fi
+if [ -e ".env" ]; then
+  set -o allexport
+    source ".env"
+  set +o allexport
+fi
 source pipenvenv
 git fetch --all --prune || true
 echo \"Origin Default: \$(git symbolic-ref refs/remotes/origin/HEAD  --short | cut -d/ -f2-)\"
